23e78295c931d220e0c79fd7e76c1fe8b2f40c91
DAMISETTI-VEERABHADRARAO <damisetti.veerabhadrarao@ibm.com>
2024-06-22 23:23:47 +0530

    .

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func heolloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method not supported", http.StatusNotFound)
 	}
+	fmt.Println("Entered hello handler")
 	fmt.Fprintf(w, "Hello......!")
 }
 
@@ -26,7 +27,9 @@ func main() {
 	http.HandleFunc("/hello", heolloHandler)
 	fmt.Println("Starting server at port 8443")
 	// You can change listening port here
+	fmt.Println("Listening on port 8443")
 	if err := http.ListenAndServe(":8443", nil); err != nil {
 		log.Fatal(err)
 	}
+
 }
